3b7d6092060bf9df49b1f855c2d8ce3653bee221
Archit <architgargagra@gmail.com>
2023-07-19 04:00:07 +0530

    Desgigned About page

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import style from "./page.module.css";
+import Image from "next/image";
+import Button from "../components/button/Button";
 const about = () => {
   return (
-    <div>
-About
-    </div>
-  )
-}
+    <div className={style.container}>
+      <div className={style.img}>
+        <Image src="/icons/photo1.jpg"   fill={true} alt="" className={style.innerimg} />
 
-export default about
+      <div className={style.text}>
+        <h1 className={style.title}> Digital Storytellers</h1>
+        <h2 className={style.desc}>
+         
+          Making best design for our clients and Users
+        </h2>
+      </div>
+      </div>
+      <div className={style.textcontainer}>
+        <div className={style.item}>
+          <h1 className={style.title}>Who Are We?</h1>
+          <p  className={style.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et velit ad in sed nostrum amet error minima adipisci accusamus corporis facilis earum laboriosam animi quae nihil illo, beatae natus sit placeat.
+            <br />
+             Quis ab adipisci itaque mollitia nam voluptatem dignissimos veritatis praesentium, est ut aperiam. Ipsam repudiandae blanditiis dolore rerum similique?</p>
+        </div>
+        <div className={style.item}>
+        <h1 className={style.title}>What We Do?</h1>
+        <p className={style.desc}>Lorem ipsum dolor sit, amet conbrsectetur adipisicing elit. Temporibus aperiam mollitia fugiat rerum nulla, pariatur deleniti eum doloribus corporis illum unde architecto consectetur nobis exercitationem, voluptas magni molestias blanditiis id vol
+<br /> 
+          uptates odio. Corrupti, eligendi. Ut cupiditate eum harum sint quidem doloribus voluptatibus, ratione aut sequi? Harum unde quo, odio qui asperiores sunt nobis. Cum nesciunt accusantium aut quod fugiat nisi.</p>
+          <Button url="/Contact" text="Contact Us"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default about;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,33 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import homeimage from 'public/icons/homepage.jpg'
+import Image from "next/image";
+import styles from "./page.module.css";
+import homeimage from "public/icons/homepage.jpg";
+import Button from "./components/button/Button";
 
 export default function Home() {
   return (
     <>
+      <div className={styles.container}>
+        <div className={styles.items}>
+          <h1 className={styles.title}>
+            Eye-Catching Design For Your Products
+          </h1>
+          <p className={styles.desc}>
+            Turning your idea into Reality. We bring together the teams from the
+            global tech industry
+          </p>
+    <Button url="/portfolio" text="See Our Works"/>
 
-    
- <div>
-  <Image src={homeimage}  width={500} height={500} alt='' className={styles.img}  />
-  
-
-  
- </div>
+        </div>
+        <div className={styles.items}>
+          <Image
+            src={homeimage}
+            width={500}
+            height={500}
+            alt=""
+            className={styles.img}
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
