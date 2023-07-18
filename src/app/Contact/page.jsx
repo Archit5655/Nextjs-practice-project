187b79e7a26b4df3cf0d21f8c5c4fcdb5c00e98b
Archit <architgargagra@gmail.com>
2023-07-19 04:28:24 +0530

    Created contact page

--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import style from './page.module.css'
+import Image from 'next/image'
+import Button from '../components/button/Button'
 
 const page = () => {
   return (
     <div className={style.container}>
-      Contact me
+      <h1 className={style.title}>
+        Contact freely for any type of issue
+      </h1>
+      <div className={style.content}>
+
+      <div className={style.img}>
+        <Image src="/icons/contact.jpg" fill={true} 
+        alt=''
+        className={style.image} />
+      </div>
+      <form className={style.form}>
+        <input type="text" placeholder='name' className={style.input} />
+        <input type="text" placeholder='email' className={style.email} />
+        <textarea className={style.textarea} placeholder='message'  cols="30" rows="10"></textarea>
+        <Button url="#" text="sumbit" />
+      </form>
+
+
+
+        </div>
     </div>
   )
 }
